fix normalization

data_loading/pytorch_dataset.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 from torch.utils.data import Dataset
-
+import numpy as np
 from .common import load_patch
 
 
@@ -132,7 +132,6 @@ class GeoLifeCLEF2022Dataset(Dataset):
         observation_id = self.observation_ids[index]
         meta = (latitude, longitude)
         patches = load_patch(observation_id, self.root, data=self.patch_data)
-        print(patches)
         # FIXME: add back landcover one hot encoding?
         # lc = patches[3]
         # lc_one_hot = np.zeros((self.one_hot_size,lc.shape[0], lc.shape[1]))
@@ -151,7 +150,7 @@ class GeoLifeCLEF2022Dataset(Dataset):
         #if len(patches) == 1:
         #    patches = patches[0]
         if "rgb" in patches and "nir" in patches:
-            patches["input"] = np.concatenate((patches["rgb"], np.expand_dims(patches["nir"], axis = -1)), axis = 2)
+            patches["input"] = np.concatenate((patches["rgb"], patches["nir"]), axis = 1)
         elif "rgb" in patches :
             patches["input"] = patches["rgb"] 
         elif "nir" in patches :

transforms/transforms.py
@@ -93,7 +93,10 @@ class Resize:
     def __call__(self, sample: Dict[str, Tensor]) -> Dict[str, Tensor]:
 
         for s in sample:
+            if len(sample[s].size())  == 2:
+                sample[s] = sample[s].unsqueeze(0).unsqueeze(0)
             sample[s] = F.interpolate(sample[s].float(), size=(self.h, self.w), mode="nearest")
+        
         return sample
 
 
@@ -107,7 +110,7 @@ class Normalize:
         self.band = band
 
     def __call__(self, sample: Dict[str, Tensor]) -> Dict[str, Tensor]:   
-        sample[band] = normalize(sample[band], means, std)
+        sample[self.band] = normalize(sample[self.band], self.means, self.std)
         return sample
 
 class RandomCrop:  # type: ignore[misc,name-defined]
@@ -136,15 +139,13 @@ class RandomCrop:  # type: ignore[misc,name-defined]
         H, W = (
             sample["rgb"].size()[-2:] if "rgb" in sample else list(sample.values())[0].size()[-2:]
         )
-        print("resizing to size")
-        print(H,W)
         if not self.center:                
             top = max(0, np.random.randint(0, max(H - self.h,1)))
             left = max(0, np.random.randint(0, max(W - self.w,1)))
         else:
             top = max(0, (H - self.h) // 2)
             left = max(0,(W - self.w) // 2)
-
+        
         return {
             task: tensor[:,:,top : top + self.h, left : left + self.w]
             for task, tensor in sample.items()