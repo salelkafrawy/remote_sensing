4c74fbb0e4647b5c4952c8286a7c38b188d3c557
sara.ebrahim-elkafrawy <sara.elkafrawy@gmail.com>
2022-07-01 17:42:04 -0400

    resolve rebase conflict

---
+++ dataset/ssl/ssl_pytorch_dataset.py
@@ -0,0 +1,127 @@
+import os
+import sys
+import inspect
+
+CURR_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+PARENT_DIR = os.path.dirname(CURR_DIR)
+sys.path.insert(0, CURR_DIR)
+
+
+from pathlib import Path
+
+import pandas as pd
+
+from torch.utils.data import Dataset
+import numpy as np
+from ssl_common import load_patch
+
+
+class GeoLifeCLEF2022Dataset(Dataset):
+    """Pytorch dataset handler for GeoLifeCLEF 2022 dataset.
+
+    Parameters
+    ----------
+    root : string or pathlib.Path
+        Root directory of dataset.
+    subset : string, either "train", "val", "train+val" or "test"
+        Use the given subset ("train+val" is the complete training data).
+    region : string, either "both", "fr" or "us"
+        Load the observations of both France and US or only a single region.
+    patch_data : string or list of string
+        Specifies what type of patch data to load, possible values: 'all', 'rgb', 'near_ir', 'landcover' or 'altitude'.
+    use_rasters : boolean (optional)
+        If True, extracts patches from environmental rasters.
+    patch_extractor : PatchExtractor object (optional)
+        Patch extractor to use if rasters are used.
+    transform : callable (optional)
+        A function/transform that takes a list of arrays and returns a transformed version.
+    target_transform : callable (optional)
+        A function/transform that takes in the target and transforms it.
+    """
+
+    def __init__(
+        self,
+        root,
+        *,
+        region="both",
+        patch_data="all",
+        use_rasters=True,
+        patch_extractor=None,
+        transform=None,
+        target_transform=None
+    ):
+        self.root = Path(root)
+        self.region = region
+        self.patch_data = patch_data
+        self.transform = transform
+        self.target_transform = target_transform
+
+        possible_regions = ["both", "fr", "us"]
+        if region not in possible_regions:
+            raise ValueError(
+                "Possible values for 'region' are: {} (given {})".format(
+                    possible_regions, region
+                )
+            )
+
+        # load the training data
+        df_train_fr = pd.read_csv(
+            self.root
+            / "observations"
+            / "observations_fr_train.csv",
+            sep=";",
+            index_col="observation_id",
+        )
+
+        df_train_us = pd.read_csv(
+            self.root
+            / "observations"
+            / "observations_us_train.csv",
+            sep=";",
+            index_col="observation_id",
+        )
+        # keep only the train (apart from the val set)
+        df_train_val = pd.concat((df_train_fr, df_train_us))
+        ind_train = df_train_val.index[df_train_val["subset"] == "train"]
+        df_train = df_train_val.loc[ind_train]
+
+        # load the test data
+        df_test_fr = pd.read_csv(
+            self.root
+            / "observations"
+            / "observations_fr_test.csv",
+            sep=";",
+            index_col="observation_id",
+        )
+        df_test_us = pd.read_csv(
+            self.root
+            / "observations"
+            / "observations_us_test.csv",
+            sep=";",
+            index_col="observation_id",
+        )
+        df_test = pd.concat((df_test_fr, df_test_us))
+        
+        # concatenate train and test data
+        df = pd.concat((df_train, df_test))
+        
+        # for debugging:
+#         df = df_test_fr.iloc[:1024]
+        self.observation_ids = df.index
+
+
+    def __len__(self):
+        return len(self.observation_ids)
+
+    
+    def __getitem__(self, index):
+        
+        observation_id = self.observation_ids[index]
+
+        patches = load_patch(observation_id, self.root, data=self.patch_data)
+
+        for band in patches.keys():
+            patches[band] = self.transform(patches[band])
+            
+        return patches
+
--- models/utils.py
+++ models/utils.py
@@ -80,7 +80,28 @@ def to_numpy(x):
 
 class InputMonitor(pl.Callback):
     
-    def on_validation_batch_start(
+#     def on_validation_batch_start(
+#         self, trainer, pl_module, batch, batch_idx, dataloader_idx
+#     ):
+
+#         if (batch_idx + 1) % trainer.log_every_n_steps == 0:
+
+#             # log inputs and targets
+#             patches, target, meta = batch
+#             input_patches = patches["input"]
+#             assert input_patches.device.type == "cuda"
+#             assert target.device.type == "cuda"
+            
+#             logger = trainer.logger
+#             logger.experiment.log_histogram_3d(
+#                 to_numpy(input_patches), "input", step=trainer.global_step
+#             )
+#             logger.experiment.log_histogram_3d(
+#                 to_numpy(target), "target", step=trainer.global_step
+#             )
+
+            
+    def on_train_batch_start(
         self, trainer, pl_module, batch, batch_idx, dataloader_idx
     ):
 
@@ -112,7 +133,6 @@ class InputMonitor(pl.Callback):
             input_patches = patches["input"]
             assert input_patches.device.type == "cuda"
             assert target.device.type == "cuda"
-#             assert meta.device.type == "cuda"
 
             logger = trainer.logger
             logger.experiment.log_histogram_3d(
@@ -121,7 +141,6 @@ class InputMonitor(pl.Callback):
             logger.experiment.log_histogram_3d(
                 to_numpy(target), "target", step=trainer.global_step
             )
-
 
 #             # log weights
 #             actual_model = next(iter(trainer.model.children()))
